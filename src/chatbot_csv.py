28c594c0f61e26877bb63331195373304099df56
eluolink <eluolink@gmail.com>
2023-04-22 22:04:59 +0400

    Update chatbot_csv.py

--- src/chatbot_csv.py
+++ src/chatbot_csv.py
@@ -33,8 +33,8 @@ path = os.path.dirname(__file__)
 key_dict = json.loads(st.secrets["textkey"])
 
 cred = credentials.Certificate(key_dict)
-firebase_admin.initialize_app(cred)
-db = firestore.Client(credentials=cred, project="cvchat")
+db = firebase_admin.initialize_app(cred)
+
 
 
 async def main():
