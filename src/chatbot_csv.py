35cc66d53b3754b8709d1a402b3e4104bef0e098
eluolink <eluolink@gmail.com>
2023-04-29 16:26:23 +0400

    Update chatbot_csv.py

--- src/chatbot_csv.py
+++ src/chatbot_csv.py
@@ -294,7 +294,8 @@ async def main():
     # Create an expander for the "About" section
     about = st.sidebar.expander("About 🤖")
     project = st.sidebar.expander("Project schema")
-    image = Image.open('schema.png')
+    path = os.path.dirname(__file__)
+    image = Image.open(path+'/schema.png')
     # Write information about the chatbot in the "About" section 
     about.write("#### If you're satisfied with the answers - you can contact Ilya via [Linkedin](https://www.linkedin.com/in/linkilya/) or ⚡[Telegram](https://t.me/eli_eth)")
     project.write(st.image(image, caption='Project schema'))
